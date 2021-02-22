53a346fe551b9531a1cf5901bd04cdbddad3c0bf
Shehan Prasanna <shehanp068@gmail.com>
2021-02-23 01:24:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const  bodyParser= require('body-parser');
+const cors= require('cors');
 
 app.use(bodyParser.json());
+app.use(cors());
 
-
+const CONNECTION_URL= 'mongodb+srv://shehan:iaOnvM7u9epNVKRa@cluster0.lhv2q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = 
